44510a93bd5202ee54433f29417e33713521567d
Joseph Manning <jgmanning3@yahoo.com>
2022-07-01 12:20:34 -0700

    Create init setup to send email verification, run success test email

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -3,4 +3,9 @@ import { signUpRoute } from "./signUpRoute";
 import { testRoute } from "./testRoute";
 import { updateUserInfoRoute } from "./updateUserInfoRoute";
 
-export const routes = [logInRoute, signUpRoute, testRoute, updateUserInfoRoute];
+export const routes = [
+  logInRoute,
+  signUpRoute,
+  testRoute,
+  updateUserInfoRoute,
+];
---
+++ back-end/src/routes/testEmailRoute.js
@@ -0,0 +1,20 @@
+import { sendEmail } from "../../util/sendEmail";
+
+export const testEmailRoute = {
+  path: "/api/test-email",
+  method: "post",
+  handler: async (req, res) => {
+    try {
+      await sendEmail({
+        to: "manning.joseph.4342+test1@gmail.com",
+        from: "manning.joseph.4342@gmail.com",
+        subject: "Does this work?",
+        text: "If you're reading this, then yes it works",
+      });
+      res.sendStatus(200);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(500);
+    }
+  },
+};
