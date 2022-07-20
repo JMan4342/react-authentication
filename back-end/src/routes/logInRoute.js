17f137432d9a518b2013fa0dbcc27c93674a8692
Joseph Manning <jgmanning3@yahoo.com>
2022-07-20 14:43:24 -0700

    Add salt and peppering to passwords. Commented out Cognito code for future reference

--- back-end/src/routes/logInRoute.js
+++ back-end/src/routes/logInRoute.js
@@ -1,13 +1,13 @@
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import {
-  AuthenticationDetails,
-  CognitoUserPool,
-  CognitoUserAttribute,
-  CognitoUser,
-} from "amazon-cognito-identity-js";
+// import {
+//   AuthenticationDetails,
+//   CognitoUserPool,
+//   CognitoUserAttribute,
+//   CognitoUser,
+// } from "amazon-cognito-identity-js";
 import { getDbConnection } from "../db.js";
-import { awsUserPool } from "../util/awsUserPool.js";
+// import { awsUserPool } from "../util/awsUserPool.js";
 
 export const logInRoute = {
   path: "/api/login",
@@ -15,32 +15,64 @@ export const logInRoute = {
   handler: async (req, res) => {
     const { email, password } = req.body;
 
-    new CognitoUser({ Username: email, Pool: awsUserPool }).authenticateUser(
-      new AuthenticationDetails({ Username: email, Password: password }),
-      {
-        onSuccess: async (result) => {
-          const db = getDbConnection("react-auth-db");
-          const user = await db.collection("users").findOne({ email });
+    // ***** AUTHENTICATION CODE ***** //
+    // ***** AUTHENTICATION CODE ***** //
+    const db = getDbConnection("react-auth-db");
+    const user = await db.collection("users").findOne({ email });
 
-          const { _id: id, isVerified, info } = user;
+    if (!user) return res.sendStatus(401);
 
-          jwt.sign(
-            { id, isVerified, email, info },
-            process.env.JWT_SECRET,
-            { expiresIn: "2d" },
-            (err, token) => {
-              if (err) {
-                res.sendStatus(500);
-              }
+    const { _id: id, isVerified, passwordHash, salt, info } = user;
+    const pepper = process.env.PEPPER_STRING;
 
-              res.status(200).json({ token });
-            }
-          );
-        },
-        onFailure: (err) => {
-          res.sendStatus(401);
-        },
-      }
-    );
+    const isCorrect = await bcrypt.compare(salt + password + pepper, passwordHash);
+
+    if (isCorrect) {
+      jwt.sign(
+        { id, isVerified, email, info },
+        process.env.JWT_SECRET,
+        { expiresIn: "2d" },
+        (err, token) => {
+          if (err) {
+            res.status(500).json(err);
+          }
+
+          res.status(200).json({ token });
+        }
+      );
+    } else {
+      res.sendStatus(401);
+    }
+
+    // ***** CONGITO AUTHENTICATION ***** //
+    // ***** CONGITO AUTHENTICATION ***** //
+
+    // new CognitoUser({ Username: email, Pool: awsUserPool }).authenticateUser(
+    //   new AuthenticationDetails({ Username: email, Password: password }),
+    //   {
+    //     onSuccess: async (result) => {
+    //       const db = getDbConnection("react-auth-db");
+    //       const user = await db.collection("users").findOne({ email });
+
+    //       const { _id: id, isVerified, info } = user;
+
+    //       jwt.sign(
+    //         { id, isVerified, email, info },
+    //         process.env.JWT_SECRET,
+    //         { expiresIn: "2d" },
+    //         (err, token) => {
+    //           if (err) {
+    //             res.sendStatus(500);
+    //           }
+
+    //           res.status(200).json({ token });
+    //         }
+    //       );
+    //     },
+    //     onFailure: (err) => {
+    //       res.sendStatus(401);
+    //     },
+    //   }
+    // );
   },
 };
--- back-end/src/routes/resetPasswordRoute.js
+++ back-end/src/routes/resetPasswordRoute.js
@@ -7,36 +7,48 @@ export const resetPasswordRoute = {
   path: "/api/users/:passwordResetCode/reset-password",
   method: "put",
   handler: async (req, res) => {
-    const { passwordResetCode } = req.params;
-    const { email, newPassword } = req.body;
 
-    new CognitoUser({ Username: email, Pool: awsUserPool }).confirmPassword(
-      passwordResetCode,
-      newPassword,
-      {
-        onSuccess: () => {
-          res.sendStatus(200);
-        },
-        onFailure: () => {
-          res.sendStatus(401);
-        },
-      }
-    );
+    // ***** AUTHENTICATION WITH COGNITO ***** //
+    // ***** AUTHENTICATION WITH COGNITO ***** //
 
-    // const db = getDbConnection("react-auth-db");
-
-    // const newPasswordHash = await bcrypt.hash(newPassword, 10);
-
-    // const result = await db.collection("users").findOneAndUpdate(
-    //   { passwordResetCode },
+    // const { passwordResetCode } = req.params;
+    // const { email, newPassword } = req.body;
+    // new CognitoUser({ Username: email, Pool: awsUserPool }).confirmPassword(
+    //   passwordResetCode,
+    //   newPassword,
     //   {
-    //     $set: { passwordHash: newPasswordHash },
-    //     $unset: { passwordResetCode: "" },
+    //     onSuccess: () => {
+    //       res.sendStatus(200);
+    //     },
+    //     onFailure: () => {
+    //       res.sendStatus(401);
+    //     },
     //   }
     // );
 
-    // if (result.lastErrorObject.n === 0) return res.sendStatus(404);
+    // ***** AUTHENTICATION CODE ***** //
+    // ***** AUTHENTICATION CODE ***** //
 
-    // res.sendStatus(200);
+    const { passwordResetCode } = req.params;
+    const { newPassword } = req.body;
+
+    const db = getDbConnection("react-auth-db");
+
+    const newSalt = uuid();
+    const pepper = process.env.PEPPER_STRING;
+
+    const newPasswordHash = await bcrypt.hash(salt + newPassword + pepper, 10);
+
+    const result = await db.collection("users").findOneAndUpdate(
+      { passwordResetCode },
+      {
+        $set: { passwordHash: newPasswordHash, salt: newSalt },
+        $unset: { passwordResetCode: "" },
+      }
+    );
+
+    if (result.lastErrorObject.n === 0) return res.sendStatus(404);
+
+    res.sendStatus(200);
   },
 };
