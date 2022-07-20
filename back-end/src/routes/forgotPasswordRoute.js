25a967337d9693c1c322cdae7acbf37c44f3fa29
Joseph Manning <jgmanning3@yahoo.com>
2022-07-20 14:08:08 -0700

    Create password reset route on backend and frontend for AWS Cognito

--- back-end/src/routes/forgotPasswordRoute.js
+++ back-end/src/routes/forgotPasswordRoute.js
@@ -1,6 +1,8 @@
-import { v4 as uuid } from "uuid";
-import { sendEmail } from "../util/sendEmail.js";
-import { getDbConnection } from "../db.js";
+// import { v4 as uuid } from "uuid";
+import { CognitoUser } from "amazon-cognito-identity-js";
+// import { sendEmail } from "../util/sendEmail.js";
+// import { getDbConnection } from "../db.js";
+import { awsUserPool } from "../util/awsUserPool.js";
 
 export const forgotPasswordRoute = {
   path: "/api/forgot-password/:email",
@@ -8,30 +10,39 @@ export const forgotPasswordRoute = {
   handler: async (req, res) => {
     const { email } = req.params;
 
-    const db = getDbConnection("react-auth-db");
-
-    const passwordResetCode = uuid();
-
-    const { result } = await db
-      .collection("users")
-      .updateOne({ email }, { $set: { passwordResetCode } });
-
-    if (result.nModified > 0) {
-      try {
-        await sendEmail({
-          to: email,
-          from: "manning.joseph.4342@gmail.com",
-          subject: "Password Reset",
-          text: `
-                    To reset your password, click this link:
-                    http://localhost:3000/reset-password/${passwordResetCode}
-                `,
-        });
-      } catch (e) {
-        console.log(e);
+    new CognitoUser({ Username: email, Pool: awsUserPool }).forgotPassword({
+      onSuccess: () => {
+        res.sendStatus(200);
+      },
+      onFailure: () => {
         res.sendStatus(500);
-      }
-    }
-    res.sendStatus(200);
+      },
+    });
+
+    // const db = getDbConnection("react-auth-db");
+
+    // const passwordResetCode = uuid();
+
+    // const { result } = await db
+    //   .collection("users")
+    //   .updateOne({ email }, { $set: { passwordResetCode } });
+
+    // if (result.nModified > 0) {
+    //   try {
+    //     await sendEmail({
+    //       to: email,
+    //       from: "manning.joseph.4342@gmail.com",
+    //       subject: "Password Reset",
+    //       text: `
+    //                 To reset your password, click this link:
+    //                 http://localhost:3000/reset-password/${passwordResetCode}
+    //             `,
+    //     });
+    //   } catch (e) {
+    //     console.log(e);
+    //     res.sendStatus(500);
+    //   }
+    // }
+    // res.sendStatus(200);
   },
 };
--- front-end/src/pages/PasswordResetLandingPage.js
+++ front-end/src/pages/PasswordResetLandingPage.js
@@ -1,19 +1,23 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import axios from "axios";
 import { PasswordResetSuccess } from "./PasswordResetSuccess";
 import { PasswordResetFail } from "./PasswordResetFail";
+import { useQueryParams } from "../util/useQueryParams";
 
 export const PasswordResetLandingPage = () => {
   const [isSuccess, setIsSuccess] = useState(false);
   const [isFailure, setIsFailure] = useState(false);
   const [passwordValue, setPasswordValue] = useState("");
   const [confirmPasswordValue, setConfirmPasswordValue] = useState("");
-  const { passwordResetCode } = useParams();
+  const [passwordResetCode, setPasswordResetCode] = useState('');
+  const {email} = useQueryParams();
+  // const { passwordResetCode } = useParams();
 
   const onResetClicked = async () => {
     try {
       await axios.put(`/api/users/${passwordResetCode}/reset-password`, {
+        email,
         newPassword: passwordValue,
       });
       setIsSuccess(true);
@@ -30,6 +34,12 @@ export const PasswordResetLandingPage = () => {
       <h1>Reset Password</h1>
       <p>Please enter a new password</p>
       <input
+        value={passwordResetCode}
+        onChange={(e) => setPasswordResetCode(e.target.value)}
+        placeholder="Password Reset Code"
+      />
+
+      <input
         type="password"
         value={passwordValue}
         onChange={(e) => setPasswordValue(e.target.value)}
@@ -41,6 +51,7 @@ export const PasswordResetLandingPage = () => {
         onChange={(e) => setConfirmPasswordValue(e.target.value)}
         placeholder="Confirm Password"
       />
+
       <button
         disabled={
           !passwordValue ||
